32e5423e34451c890e17750567624b7e12f47c86
dearcandy <18292169782@163.com>
2022-11-29 15:23:50 +0800

    Feature article服务添加测试类 测试文章内容生成html上传minio

---
+++ heima-leadnews-service/heima-leadnews-article/src/test/java/com.heima.article/ArticleFreemarkerTest.java
@@ -0,0 +1,77 @@
+package com.heima.article;
+
+import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.heima.article.mapper.ApArticleContentMapper;
+import com.heima.article.mapper.ApArticleMapper;
+import com.heima.file.service.FileStorageService;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleContent;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ArticleFreemarkerTest {
+
+    @Resource
+    private Configuration configuration;
+    @Resource
+    private FileStorageService fileStorageService;
+    @Resource
+    private ApArticleMapper apArticleMapper;
+    @Resource
+    private ApArticleContentMapper apArticleContentMapper;
+
+    @Test
+    public void createStaticUrlTest() throws IOException, TemplateException {
+        // 获取文章内容
+        LambdaQueryWrapper<ApArticleContent> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(ApArticleContent::getArticleId, 1302862387124125698L);
+        ApArticleContent apArticleContent = apArticleContentMapper.selectOne(wrapper);
+
+        if (apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())){
+
+            StringWriter out = new StringWriter();
+            // 文章内容通过freemarker生成html文件
+            Template template = configuration.getTemplate("article.ftl");
+
+            // 构造数据
+            Map<String, Object> params = new HashMap<>();
+            params.put("content", JSONArray.parseArray(apArticleContent.getContent()));
+
+            // 生成文件流
+            template.process(params, out);
+            InputStream is = new ByteArrayInputStream(out.toString().getBytes());
+
+            // 上传html文件到minio
+            String path = fileStorageService.uploadHtmlFile("", apArticleContent.getArticleId() + ".html", is);
+
+            // 修改ap_article表 保存static_url字段
+            ApArticle apArticle = new ApArticle();
+            apArticle.setId(apArticleContent.getArticleId());
+            apArticle.setStaticUrl(path);
+            int i = apArticleMapper.updateById(apArticle);
+
+            System.out.println("更新成功条数 = " +  i);
+
+        }
+
+    }
+}
--- heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/MinIOTest.java
@@ -19,36 +19,36 @@ public class MinIOTest {
     @Resource
     private FileStorageService fileStorageService;
 
-    @Test
-    public void test() throws FileNotFoundException {
-        FileInputStream fileInputStream =  new FileInputStream("D:\\list.html");;
-        String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
-        System.out.println(path);
-    }
-
-//    public static void main(String[] args) {
-//
-//        FileInputStream fileInputStream;
-//        try {
-//
-//            fileInputStream =  new FileInputStream("D:\\list.html");;
-//
-//            //1.创建minio链接客户端
-//            MinioClient minioClient = MinioClient.builder().credentials("admin", "admin123456").endpoint("http://121.4.65.89:9000").build();
-//            //2.上传
-//            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
-//                    .object("list.html")//文件名
-//                    .contentType("text/html")//文件类型
-//                    .bucket("leadnews")//桶名词  与minio创建的名词一致
-//                    .stream(fileInputStream, fileInputStream.available(), -1) //文件流
-//                    .build();
-//            minioClient.putObject(putObjectArgs);
-//
-//            System.out.println("http://121.4.65.89:9090/leadnews/list.html");
-//
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
+//    @Test
+//    public void test() throws FileNotFoundException {
+//        FileInputStream fileInputStream =  new FileInputStream("D:\\tmp\\css\\index.css");;
+//        String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
+//        System.out.println(path);
 //    }
+
+    public static void main(String[] args) {
+
+        FileInputStream fileInputStream;
+        try {
+
+            fileInputStream =  new FileInputStream("D:\\tmp\\js\\axios.min.js");;
+
+            //1.创建minio链接客户端
+            MinioClient minioClient = MinioClient.builder().credentials("admin", "admin123456").endpoint("http://121.4.65.89:9000").build();
+            //2.上传
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object("plugins/js/axios.min.js")//文件名
+                    .contentType("text/js")//文件类型
+                    .bucket("leadnews")//桶名词  与minio创建的名词一致
+                    .stream(fileInputStream, fileInputStream.available(), -1) //文件流
+                    .build();
+            minioClient.putObject(putObjectArgs);
+
+            //System.out.println("http://121.4.65.89:9090/leadnews/list.html");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
