97aaa4d2ca64195c2ee50402a6e374fc316cd15e
dearcandy <18292169782@163.com>
2022-12-07 23:34:19 +0800

    ADD Kafka-demo 测试单节点消息发送与消费

---
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/simple/ConsumerQuickStart.java
@@ -0,0 +1,43 @@
+package com.heima.kafka.simple;
+
+import org.apache.kafka.clients.consumer.*;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * 消费者
+ */
+public class ConsumerQuickStart {
+
+    public static void main(String[] args) {
+        // kafka 连接配置信息
+        Properties props = new Properties();
+
+        // 设置连接地址
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "121.4.65.89:9092");
+        // key和value的反序列化器
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        // 消费者组
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group2");
+
+        // 创建消费者对象
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        // 订阅主题
+        consumer.subscribe(Collections.singletonList("topic-first"));
+
+        while (true){
+            // 拉取消息
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            consumerRecords.forEach(item->{
+                System.out.println(item.key());
+                System.out.println(item.value());
+            });
+        }
+
+    }
+
+}
---
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/simple/ProducerQuickStart.java
@@ -0,0 +1,36 @@
+package com.heima.kafka.simple;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * 生产者
+ */
+public class ProducerQuickStart {
+
+    public static void main(String[] args) {
+        // kafka 连接配置信息
+        Properties props = new Properties();
+
+        // 设置连接地址
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "121.4.65.89:9092");
+        // key和value的序列化器
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // 创建kafka生产者
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        // 发送消息
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("topic-first", "key-001", "value-001");
+        producer.send(producerRecord);
+
+        // 关闭消息通道 必须关闭
+        producer.close();
+    }
+
+}
