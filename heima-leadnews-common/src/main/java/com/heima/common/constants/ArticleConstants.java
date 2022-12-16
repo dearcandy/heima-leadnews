ec7493fe0162e83e87b347950ff2a3e45df607cb
dearcandy <18292169782@163.com>
2022-12-16 10:56:13 +0800

    Feature 新增文章时创建索引

--- heima-leadnews-common/src/main/java/com/heima/common/constants/ArticleConstants.java
+++ heima-leadnews-common/src/main/java/com/heima/common/constants/ArticleConstants.java
@@ -17,4 +17,6 @@ public class ArticleConstants {
      */
     public static final String DEFAULT_TAG = "__all__";
 
+    public static final String ARTICLE_ES_SYNC_TOPIC = "article.es.sync.topic";
+
 }
--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
@@ -1,16 +1,21 @@
 package com.heima.article.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.heima.article.mapper.ApArticleContentMapper;
 import com.heima.article.service.ApArticleService;
 import com.heima.article.service.ArticleFreemarkerService;
+import com.heima.common.constants.ArticleConstants;
 import com.heima.file.service.FileStorageService;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.search.vos.SearchArticleVo;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,6 +43,9 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
 
     @Resource
     private ApArticleService apArticleService;
+
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     /**
      * 生成静态文件上传到minIO中
@@ -74,9 +82,25 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
             apArticleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId,apArticle.getId())
                     .set(ApArticle::getStaticUrl,path));
 
-
+            // 发送消息 创建索引
+            createArticleEsIndex(apArticle, content, path);
         }
 
 
+    }
+
+    /**
+     * 发送消息 创建索引
+     * @param apArticle 文章实体
+     * @param content 文章内容
+     * @param path 文章模板静态URL
+     */
+    private void createArticleEsIndex(ApArticle apArticle, String content, String path) {
+        SearchArticleVo searchArticleVo = new SearchArticleVo();
+        BeanUtils.copyProperties(apArticle, searchArticleVo);
+        searchArticleVo.setContent(content);
+        searchArticleVo.setStaticUrl(path);
+
+        kafkaTemplate.send(ArticleConstants.ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(searchArticleVo));
     }
 }
